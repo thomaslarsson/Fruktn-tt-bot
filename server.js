06abf04a154361869d1c8ca2349fc72ddbb7a9ae
Thomas Maurstad Larsson <thomas.m.larsson@gmail.com>
2022-03-08 14:57:50 +0100

    Changes config for web part to work

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 import express from 'express';
 
-// Serve on PORT on Heroku and on localhost:5000 locally
 const PORT = process.env.PORT || '5000';
 const app = express();
 
-// Serve the two static assets
-app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }));
+// Use the public folder
+app.use(express.static('public'));
 app.listen(PORT, () => console.log("Server started!"));
